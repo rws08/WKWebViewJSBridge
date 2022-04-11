439462bd5a2693eec9700c93c055d5964b00bd0f
rws08 <won.ryu@groundx.xyz>
2022-04-11 15:07:14 +0900

    테스트 추가중

--- WKWebViewJSBridge/Tests/WKWebViewJSBridgeTests/Utils.swift
+++ WKWebViewJSBridge/Tests/WKWebViewJSBridgeTests/Utils.swift
@@ -1,8 +1,34 @@
 //
-//  File.swift
+//  Utils.swift
 //  
 //
 //  Created by won on 2022/04/07.
 //
+// Source : https://digitalbunker.dev/how-to-test-private-methods-variables-in-swift/
+//
 
 import Foundation
+
+class MirrorObject {
+    let mirror: Mirror
+    
+    init(reflecting: Any) {
+        mirror = Mirror(reflecting: reflecting)
+    }
+    
+    func extract<T>(variableName: StaticString = #function) -> T? {
+        extract(variableName: variableName, mirror: mirror)
+    }
+    
+    private func extract<T>(variableName: StaticString, mirror: Mirror?) -> T? {
+        guard let mirror = mirror else {
+            return nil
+        }
+        
+        guard let descendant = mirror.descendant("\(variableName)") as? T else {
+            return extract(variableName: variableName, mirror: mirror.superclassMirror)
+        }
+        
+        return descendant
+    }
+}
--- WKWebViewJSBridge/Tests/WKWebViewJSBridgeTests/WKWebViewJSBridgeTests.swift
+++ WKWebViewJSBridge/Tests/WKWebViewJSBridgeTests/WKWebViewJSBridgeTests.swift
@@ -2,10 +2,49 @@ import XCTest
 @testable import WKWebViewJSBridge
 
 final class WKWebViewJSBridgeTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(WKWebViewJSBridge().text, "Hello, World!")
+    func test_requestWeb() {
+        let bridge = WKWebViewJSBridge()
+        let bridgeMirror = WKWebViewJSBridgeMirror(bridge)
+        bridge.requestWeb()
+        XCTAssertEqual(bridgeMirror.receiverPool?.count, 0)
+        bridge.requestWeb(receiver: { _, _ in })
+        XCTAssertEqual(bridgeMirror.receiverPool?.count, 1)
+    }
+    
+    func test_addReceiverHandler() {
+        let bridge = WKWebViewJSBridge()
+        let bridgeMirror = WKWebViewJSBridgeMirror(bridge)
+        var actual = bridge
+            .addReceiverHandler(name: "test0", {_,_ in })
+        XCTAssertEqual(actual, bridge)
+        XCTAssertEqual(bridgeMirror.receiverPool?.count, 1)
+        actual = bridge
+            .addReceiverHandler(name: "test0", {_,_ in })
+            .addReceiverHandler(name: "test1", {_,_ in })
+        XCTAssertEqual(actual, bridge)
+        XCTAssertEqual(bridgeMirror.receiverPool?.count, 2)
+        actual = bridge
+            .addReceiverHandler(name: "test2", {_,_ in })
+        XCTAssertEqual(actual, bridge)
+        XCTAssertEqual(bridgeMirror.receiverPool?.count, 3)
+    }
+    
+    func test_isNeedReceive() {
+        var actual = WKWebViewJSBridge.isNeedReceive(["requestIDWeb":"test"])
+        XCTAssertTrue(actual)
+        actual = WKWebViewJSBridge.isNeedReceive(["requestIDWeb":""])
+        XCTAssertFalse(actual)
+        actual = WKWebViewJSBridge.isNeedReceive(["":""])
+        XCTAssertFalse(actual)
+    }
+}
+
+class WKWebViewJSBridgeMirror: MirrorObject {
+    init(_ instance: WKWebViewJSBridge) {
+        super.init(reflecting: instance)
+    }
+    // List all private properties you wish to test using SAME NAME.
+    var receiverPool: [String: JSBReceiver]? {
+        extract()
     }
 }
